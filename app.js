9d711c34f4454ef22698ba0514ba52e0ddd16114
Chris Thomas <webmaster102@gmail.com>
2019-05-06 23:33:56 -0400

    added new version with clean es6

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-function emo1() { alert("I'm Happy!"); }
-function emo2() { alert("I'm Sad!"); }
-function emo3() { alert("I'm Mad!"); }
-function emo4() { alert("I'm Tired!"); }
-function emo5() { alert("I'm Hungry and Thirsty!"); }
-function emo6() { alert("I'm Sick!"); }
+var happy = document.querySelector(".happy");
+var sad = document.querySelector(".sad");
+var mad = document.querySelector(".mad");
+var tired = document.querySelector(".tired");
+var hungry = document.querySelector(".hungry");
+var sick = document.querySelector(".sick");
+happy.addEventListener("click", () => alert("I'm " + happy.innerHTML));
+sad.addEventListener("click", () => alert("I'm " + sad.innerHTML));
+mad.addEventListener("click", () => alert("I'm " + mad.innerHTML));
+tired.addEventListener("click", () => alert("I'm " + tired.innerHTML));
+hungry.addEventListener("click", () => alert("I'm " + hungry.innerHTML));
+sick.addEventListener("click", () => alert("I'm " + sick.innerHTML));
 
 if('serviceWorker' in navigator) {
 	navigator.serviceWorker
