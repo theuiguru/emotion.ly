a40cb7ef2b7d7fe66cee4a3ace278ae3a2ba5d61
Thomas <cthoma07@chsli.org>
2020-07-01 23:20:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+setTimeout(function() {
+  document.querySelector('.splash').style.display = 'none';
+  document.querySelector('.main').style.display = 'grid';
+}, 2000);
 const happy = document.querySelector(".happy"),
       sad = document.querySelector(".sad"),
       mad = document.querySelector(".mad"),
@@ -10,7 +14,3 @@ mad.addEventListener("click", () => alert("I'm " + mad.innerHTML));
 tired.addEventListener("click", () => alert("I'm " + tired.innerHTML));
 hungry.addEventListener("click", () => alert("I'm " + hungry.innerHTML));
 sick.addEventListener("click", () => alert("I'm " + sick.innerHTML));
-setTimeout(function() {
-  document.querySelector('.splash').style.display = 'none';
-  document.querySelector('.main').style.display = 'grid';
-}, 2000);
