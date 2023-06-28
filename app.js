71b08f008eb7ef302faa26deca743e4912db52d0
Chris Thomas <webmaster102@gmail.com>
2023-06-28 12:33:37 -0400

    Happy 11th anniversary!
    dialog modal added instead of alert()

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const year = document.querySelector('.year').textContent = new Date().getFullYear();
-let happy = document.querySelector(".happy"),
-      sad = document.querySelector(".sad"),
-      mad = document.querySelector(".mad"),
-      tired = document.querySelector(".tired"),
-      hungry = document.querySelector(".hungry"),
-      sick = document.querySelector(".sick");
-happy.addEventListener("click", () => alert(`I'm ${happy.textContent}`));
-sad.addEventListener("click", () => alert(`I'm ${sad.textContent}`));
-mad.addEventListener("click", () => alert(`I'm ${mad.textContent}`));
-tired.addEventListener("click", () => alert(`I'm ${tired.textContent}`));
-hungry.addEventListener("click", () => alert(`I'm ${hungry.textContent}`));
-sick.addEventListener("click", () => alert(`I'm ${sick.textContent}`));
+const dialogTriggers = document.querySelectorAll('.emotion');
+const dialog = document.getElementById('emotion');
+
+// Add click event listeners to all the <a> tags
+dialogTriggers.forEach(trigger => {
+  trigger.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    dialog.classList.add('open');
+
+    const emotion = trigger.classList[1];
+
+    dialog.innerHTML = `I'm ${emotion} <br><button onclick="dialog.close();">Close</button>`;
+
+    const backgroundColor = getComputedStyle(trigger).backgroundColor;
+    dialog.style.backgroundColor = backgroundColor;
+    
+    dialog.showModal();
+  });
+});
