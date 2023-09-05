747eb021f743bf6417dae9a0c6fcb42c8a344b5b
Chris Thomas <webmaster102@gmail.com>
2023-09-04 22:26:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ const dialog = document.getElementById('emotion');
 dialogTriggers.forEach(trigger => {
   trigger.addEventListener('click', (event) => {
     event.preventDefault();
-
     dialog.classList.add('open');
-
     const emotion = trigger.classList[1];
-
-    dialog.innerHTML = `I'm ${emotion} <br><button onclick="dialog.close();">Close</button>`;
-
+    dialog.innerHTML = `<button onclick="dialog.close();" class="closeModal">
+      <svg xmlns="http://www.w3.org/2000/svg" class="closeModalX" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M6.293 6.293a1 1 0 0 1 1.414 0L10 8.586l2.293-2.293a1 1 0 1 1 1.414 1.414L11.414 10l2.293 2.293a1 1 0 0 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 0-1.414z"/>
+      </svg>
+    </button> I'm ${emotion}`;
     const backgroundColor = getComputedStyle(trigger).backgroundColor;
     dialog.style.backgroundColor = backgroundColor;
-    
     dialog.showModal();
   });
+});
+dialog.addEventListener('click', (event) => {
+  if (event.target === dialog) { dialog.close(); }
 });
